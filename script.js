c888335c5b55c58ac30b4037dd7d8246463ac4e3
brunoro7 <bruno.ro7c@gmail.com>
2021-12-26 15:08:53 -0300

    colorindo o board-pixel pelo js.

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 let tamanhoBoxPixels = 5;
 
+/** ======= Colorindo a paleta de cores ======= */
+
+  let pixelBlack = document.querySelector(".selectBlack");
+  pixelBlack.style.backgroundColor = "black";
+
+  let pixelBlue = document.querySelector(".selectBlue");
+  pixelBlue.style.backgroundColor = "blue";
+
+  let pixelRed = document.querySelector(".selectRed");
+  pixelRed.style.backgroundColor = "red";
+
+  let pixelGreen = document.querySelector(".selectGreen");
+  pixelGreen.style.backgroundColor = "green";
+
+
 // let boxPixels = document.;
 let cadaPixel = document.querySelector(".pixel");
 
 /** ======= Função que cria a linha com os Pixels ======= */
 
-let cadaLine = document.getElementById("pixelOnLine");
 for(let pixelInline = 0; pixelInline < tamanhoBoxPixels; pixelInline += 1){
   let recebeCadaPixel = document.createElement("div");
   recebeCadaPixel.className = "pixel";
-  document.querySelector("#pixelOnLine").appendChild(recebeCadaPixel);
-  for(let lineOfPixel = 0; lineOfPixel < tamanhoBoxPixels; lineOfPixel += 1){
-    let recebeCadaLine = document.createElement("div");
-
-    // recebeCadaLine.className = "linePixel";
-
-    document.querySelector("#lineOfPixel").appendChild(recebeCadaLine);
-    };
+  document.querySelector("#pixelOnLine").appendChild(recebeCadaPixel);    
+  
 };
+
+// let cadaLine = document.querySelector("section#pixelOnLine");
+// for(let lineOfPixel of lineOfPixel){
+
+//   let recebeCadaLine = document.createElement("div");
+//     document.querySelector("#lineOfPixel").appendChild(recebeCadaLine);
+    
+// }
 
 
 
