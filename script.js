a8f07d968de299d19fcbd54e2b84f901f26ecf08
brunoro7 <bruno.ro7c@gmail.com>
2021-12-27 02:13:03 -0300

    tentando entender pq o test ainda não reconhece as quantidades de linha e coluna.

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
+/** ======= Vai receber o número do usuário mais pra frente ======= */
+
 let tamanhoBoxPixels = 5;
-// let boxBoardPixel = document.querySelector(".tamanhoBoardPixel");
-//      boxBoardPixel.style.width = ;
-//      boxBoardPixel.style.heigth = ;
 
 /** ======= Colorindo a paleta de cores ======= */
 
   let pixelBlack = document.querySelector("li.color.first");
-    pixelBlack.className = "pixel color selectBlack";
+    pixelBlack.className = "pixel color first";
 
   let pixelBlue = document.querySelector("li.color.second");
-    pixelBlue.className = "pixel color selectBlue";
+    pixelBlue.className = "pixel color second";
 
   let pixelRed = document.querySelector("li.color.third");
-    pixelRed.className = "pixel color selectRed";
+    pixelRed.className = "pixel color third";
 
   let pixelGreen = document.querySelector("li.color.four");
-    pixelGreen.className = "pixel color selectGreen";
-
+    pixelGreen.className = "pixel color four";
     
 /** ======= Função que cria a tabela de Pixels ======= */
-
 
 for(let pixelInline = 0; pixelInline < tamanhoBoxPixels; pixelInline += 1){
   let recebeCadaPixel = document.createElement("div");
@@ -33,34 +30,28 @@ for(let pixelInline = 0; pixelInline < tamanhoBoxPixels; pixelInline += 1){
   };
 }
 
+/** ======= Cálculo do Tamanho do 'quadro' de pixels ======= */
 
+  let quantDivs = document.getElementsByTagName("div");
+  let numOfDivs = 0;
+  for(let contaDivs = 1; contaDivs <= quantDivs.length; contaDivs += 1){
+    numOfDivs += 1;
+  }
+  
+  let calcQtdDivs = numOfDivs/tamanhoBoxPixels;
+  let calcDoTamanho = (tamanhoBoxPixels * tamanhoBoxPixels);
+  if (calcQtdDivs === tamanhoBoxPixels){
+    let boxBoardPixel = document.querySelector(".tamanhoBoardPixel");
+    boxBoardPixel.style.width = ((calcDoTamanho*10) + "px");
+  };
 
+/** ======= definir ======= */
 
-// let cadaLine = document.querySelector("section#pixelOnLine");
-// for(let lineOfPixel of lineOfPixel){
+// let colorSelectedAtual = colorBlack;
 
-//   let recebeCadaLine = document.createElement("div");
-//     document.querySelector("#lineOfPixel").appendChild(recebeCadaLine);
-    
+// function colorSelected(clickNaPaleta) {
+  
+  // if(clickNaPaleta === selectedAtual){
+  // selectedAtual = selectedAtual;
+  // }
 // }
-
-
-
-
-
-
-  /** ======= Função que imprimi a quantidade de linhas ======= */
-
-/** ======= Função que defini a cor preta como inicial 'selected' ======= */
-
-/** ======= Função que escolhe a cor ======= */
-
-/** ======= Função que pinta ======= */
-
-/** ======= Função que reseta o quadro para branco ======= */
-
-/** ======= Função que defini o tamanho do quadro de 5 à 50 ======= */
-
-/** ======= Função que escolhe a cor ======= */
-
-/** ======= Função que escolhe cores aleatorias no carregamento ======= */
