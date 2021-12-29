fcd3e612e2e068dfbe3776f73ff450a02fd5b62b
brunoro7 <bruno.ro7c@gmail.com>
2021-12-29 18:07:44 -0300

    criando quadro de pixels 'manipulação de acordo com a entrada do tamanhdoDoBox'.

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
+/** ======= Const's e Let's importantes ======= */
+const pixelBoard = document.getElementById("pixel-board");
+
 /** ======= Vai receber o número do usuário mais pra frente ======= */
 
 let tamanhoBoxPixels = 5;
+
 const linhaBoxPixels = tamanhoBoxPixels;
 const colunaBoxPixels = tamanhoBoxPixels;
 
 /** ======= Cada Cor Da Paleta ======= */
 
-  let pixelBlack = document.getElementsByTagName("li")[0];
+const pixelBlack = document.getElementsByTagName("li")[0];
 
-  let pixelBlue = document.getElementsByTagName("li")[1];
+const pixelBlue = document.getElementsByTagName("li")[1];
 
-  let pixelRed = document.getElementsByTagName("li")[2];
+const pixelRed = document.getElementsByTagName("li")[2];
 
-  let pixelGreen = document.getElementsByTagName("li")[3];
-    
+const pixelGreen = document.getElementsByTagName("li")[3];
+
 /** ======= Função que cria a tabela de Pixels ======= */
+function QuadroPixel (){
 
-  for(let indexCadaColuna = 0; indexCadaColuna < colunaBoxPixels; indexCadaColuna += 1 ){
-
-    for(let pixelInline = 0; pixelInline < linhaBoxPixels; pixelInline += 1){    
+  for(let indexCadaColuna = 0; indexCadaColuna < tamanhoBoxPixels; indexCadaColuna += 1 ){
     
-    let recebeCadaColuna = document.createElement("div");
+    for(let pixelInline = 0; pixelInline < tamanhoBoxPixels; pixelInline += 1){    
+      
+      let recebeCadaColuna = document.createElement("div");
       recebeCadaColuna.className = "pixel";
-      document.querySelector("#pixel-board").appendChild(recebeCadaColuna);
-
-    let recebePixelInline = document.createElement("div");
-    recebePixelInline.className = "pixel";
-    recebeCadaColuna.appendChild(recebePixelInline);
-  };
-}
-
+      pixelBoard.appendChild(recebeCadaColuna);
+      
+      let recebePixelInline = document.createElement("div");
+      recebePixelInline.className = "pixel";
+      pixelBoard.appendChild(recebePixelInline);
+    };
+  }
 /** ======= Cálculo do Tamanho do 'quadro' de pixels ======= */
+  const tamanhoPixel = 42;
+  const tamanhoLinhaEmPx = tamanhoPixel*tamanhoBoxPixels;
+    pixelBoard.style.width = tamanhoLinhaEmPx + "px";
+}
+QuadroPixel()
 
-  // let quantDivs = document.getElementsByTagName("div");
-  // let numOfDivs = 0;
-  // for(let contaDivs = 1; contaDivs <= quantDivs.length; contaDivs += 1){
-  //   numOfDivs += 1;
-  // }
-  
-  // let calcQtdDivs = numOfDivs/tamanhoBoxPixels;
-  // let calcDoTamanho = (tamanhoBoxPixels * tamanhoBoxPixels);
-  // if (calcQtdDivs === tamanhoBoxPixels){
-  //   let boxBoardPixel = document.querySelector(".tamanhoBoardPixel");
-  //   boxBoardPixel.style.width = ((calcDoTamanho*10) + "px");
-  // };
 
 /** ======= definir ======= */
 
