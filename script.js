295ba4f3a6df13e4aff3c8fee53ee9c3d085733d
brunoro7 <bruno.ro7c@gmail.com>
2021-12-29 16:30:26 -0300

    refatorando codigo para tentar facilitar os acessos.

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
 /** ======= Vai receber o número do usuário mais pra frente ======= */
 
 let tamanhoBoxPixels = 5;
+const linhaBoxPixels = tamanhoBoxPixels;
+const colunaBoxPixels = tamanhoBoxPixels;
 
 /** ======= Colorindo a paleta de cores ======= */
 
-  let pixelBlack = document.querySelector("li.color.first");
-    pixelBlack.className = "pixel color first";
+  let pixelBlack = document.getElementsByTagName("li")[0];
+    pixelBlack.className = "colorBlack";
 
-  let pixelBlue = document.querySelector("li.color.second");
-    pixelBlue.className = "pixel color second";
+  let pixelBlue = document.getElementsByTagName("li")[1];
+    pixelBlue.className = "colorBlue";
 
-  let pixelRed = document.querySelector("li.color.third");
-    pixelRed.className = "pixel color third";
+  let pixelRed = document.getElementsByTagName("li")[2];
+    pixelRed.className = "colorRed";
 
-  let pixelGreen = document.querySelector("li.color.four");
-    pixelGreen.className = "pixel color four";
+  let pixelGreen = document.getElementsByTagName("li")[3];
+    pixelGreen.className = "colorGreen";
     
 /** ======= Função que cria a tabela de Pixels ======= */
 
-for(let pixelInline = 0; pixelInline < tamanhoBoxPixels; pixelInline += 1){
-  let recebeCadaPixel = document.createElement("div");
-  recebeCadaPixel.className = "pixel";
-  document.querySelector("#pixel-board").appendChild(recebeCadaPixel);
+  for(let indexCadaColuna = 0; indexCadaColuna < colunaBoxPixels; indexCadaColuna += 1 ){
 
-  for(let indexCadaLinha = 0; indexCadaLinha < tamanhoBoxPixels-1; indexCadaLinha += 1 ){
-    let recebeCadaLine = document.createElement("div");
-    recebeCadaLine.className = "pixel";
-    document.querySelector("#pixel-board").appendChild(recebeCadaLine);
+    for(let pixelInline = 0; pixelInline < linhaBoxPixels; pixelInline += 1){    
+    
+    let recebeCadaColuna = document.createElement("div");
+      recebeCadaColuna.className = "pixel";
+      document.querySelector("#pixel-board").appendChild(recebeCadaColuna);
+
+    let recebePixelInline = document.createElement("div");
+    recebePixelInline.className = "pixel";
+    recebeCadaColuna.appendChild(recebePixelInline);
   };
 }
 
 /** ======= Cálculo do Tamanho do 'quadro' de pixels ======= */
 
-  let quantDivs = document.getElementsByTagName("div");
-  let numOfDivs = 0;
-  for(let contaDivs = 1; contaDivs <= quantDivs.length; contaDivs += 1){
-    numOfDivs += 1;
-  }
+  // let quantDivs = document.getElementsByTagName("div");
+  // let numOfDivs = 0;
+  // for(let contaDivs = 1; contaDivs <= quantDivs.length; contaDivs += 1){
+  //   numOfDivs += 1;
+  // }
   
-  let calcQtdDivs = numOfDivs/tamanhoBoxPixels;
-  let calcDoTamanho = (tamanhoBoxPixels * tamanhoBoxPixels);
-  if (calcQtdDivs === tamanhoBoxPixels){
-    let boxBoardPixel = document.querySelector(".tamanhoBoardPixel");
-    boxBoardPixel.style.width = ((calcDoTamanho*10) + "px");
-  };
+  // let calcQtdDivs = numOfDivs/tamanhoBoxPixels;
+  // let calcDoTamanho = (tamanhoBoxPixels * tamanhoBoxPixels);
+  // if (calcQtdDivs === tamanhoBoxPixels){
+  //   let boxBoardPixel = document.querySelector(".tamanhoBoardPixel");
+  //   boxBoardPixel.style.width = ((calcDoTamanho*10) + "px");
+  // };
 
 /** ======= definir ======= */
 
