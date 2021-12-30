c777a89409d4a94513f51b0f1f7eff2e4379c1f2
brunoro7 <bruno.ro7c@gmail.com>
2021-12-30 05:03:16 -0300

    ainda tentando resolver o requisito 8 e 9.

--- script.js
+++ script.js
@@ -20,17 +20,6 @@ const pixelBlue = document.getElementsByTagName("span")[1].style;
 const pixelRed = document.getElementsByTagName("span")[2].style;
 const pixelGreen = document.getElementsByTagName("span")[3].style;
 
-/** ======= Defini a classe .selected ======= */
-
-function colorSelected (event){
-  
-  const classeSelected = document.getElementsByClassName("selected")[0];
-
-    classeSelected.classList.remove("selected");
-    event.target.classList.toggle("selected");
-  }
-colorPalette.addEventListener("click", colorSelected);
-
 /** ======= Função que cria a tabela de Pixels ======= */
 
 function quadroPixel (){
@@ -38,13 +27,13 @@ function quadroPixel (){
   for(let indexCadaColuna = 0; indexCadaColuna < tamanhoBoxPixels; indexCadaColuna += 1 ){
     
     let recebeCadaColuna = document.createElement("div");
-    recebeCadaColuna.className = "pixel";
+    recebeCadaColuna.className = "pixel color";
     pixelBoard.appendChild(recebeCadaColuna);
 
     //obs: pixelInLine poderia começar no 1, e retirar o -1 do tamanhoBoxPixels;
     for(let pixelInline = 0; pixelInline < tamanhoBoxPixels -1; pixelInline += 1){
       let recebePixelInline = document.createElement("div");
-        recebePixelInline.className = "pixel";
+        recebePixelInline.className = "pixel color";
         pixelBoard.appendChild(recebePixelInline);
     };
   }
@@ -55,24 +44,37 @@ function quadroPixel (){
 }
 quadroPixel();
 
+/** ======= Defini a classe .selected ======= */
+
+function colorSelected (event){
+  const classeSelected = document.getElementsByClassName("selected")[0];
+
+    classeSelected.classList.remove("selected");
+    //Nesse caso, no lugar do toggle poderia usar o add;
+    event.target.classList.toggle("selected");
+  }
+colorPalette.addEventListener("click", colorSelected);
+
 /** ======= Função Que Pinta Cada Pixel ======= */
 
 // function colorePixel (event){
+
+//   const pixelClicado = event.target;
+//   let pixelSelecionadoColorido = document.querySelector(".selected");
   
-//   let pixelSelecionadoColorido = document.querySelector("span .selected");
-  
-//   event.target.style.backgroundColor = pixelSelecionadoColorido.style.backgroundColor;
+//   pixelClicado.style.backgroundColor = pixelSelecionadoColorido.style.backgroundColor;
 // }
 // pixelBoard.addEventListener("click", colorePixel);
 
 /** ======= Função Do Button Clear ======= */
 
+//funciona, mas não passa no teste;
 function clearBoard (){
 
-  const listaPixelBoard = pixelBoard.children;
+  const listaPixelBoard = document.querySelectorAll(".pixel");
   
-  for(let i of listaPixelBoard){
-    i.style.backgroundColor = "white";
+  for(let i = 0; i < listaPixelBoard.length; i += 1){
+    listaPixelBoard[i].style.backgroundColor = "orange";
   }
 }
 buttonClear.addEventListener("click", clearBoard);
