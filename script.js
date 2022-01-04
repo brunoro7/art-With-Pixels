324b8b51c2f3e094a0e3683d54640bf88960f246
brunoro7 <bruno.ro7c@gmail.com>
2022-01-04 04:11:52 -0300

    criandoa base para as cores aleatorias.

--- script.js
+++ script.js
@@ -13,9 +13,44 @@ const valorInput = document.getElementById("board-size");
 /** ======= Cada Cor Da Paleta ======= */
 
 const pixelBlack = document.getElementsByTagName("span")[0].style;
-const pixelBlue = document.getElementsByTagName("span")[1].style;
-const pixelRed = document.getElementsByTagName("span")[2].style;
-const pixelGreen = document.getElementsByTagName("span")[3].style;
+
+let pixelRandom1 = document.getElementById("colorRandom1").style;
+
+let pixelRandom2 = document.getElementById("colorRandom2").style;
+
+let pixelRandom3 = document.getElementById("colorRandom3").style;
+
+/** ======= Função que cria cores aleatórias ======= */
+
+let numAle1 = Math.floor(Math.random()*255);
+let numAle2 = Math.floor(Math.random()*255);
+let numAle3 = Math.floor(Math.random()*255);
+let montandoTrio1 = [numAle1, numAle2, numAle3];
+let trioNumber1 = montandoTrio1.join(", ");
+
+let numAle4 = Math.floor(Math.random()*255);
+let numAle5 = Math.floor(Math.random()*255);
+let numAle6 = Math.floor(Math.random()*255);
+let montandoTrio2 = [numAle4, numAle5, numAle6];
+let trioNumber2 = montandoTrio2.join(", ");
+
+let numAle7 = Math.floor(Math.random()*255);
+let numAle8 = Math.floor(Math.random()*255);
+let numAle9 = Math.floor(Math.random()*255);
+let montandoTrio3 = [numAle7, numAle8, numAle9];
+let trioNumber3 = montandoTrio3.join(", ");
+
+console.log("trio1");
+console.log(numAle1, numAle2, numAle3);
+console.log(trioNumber1);
+
+console.log("trio2");
+console.log(numAle1, numAle2, numAle3);
+console.log(trioNumber2);
+
+console.log("trio3");
+console.log(numAle1, numAle2, numAle3);
+console.log(trioNumber3);
 
 /** ======= Const do Botao e Input de tamanho do quadro ======= */
 
@@ -102,15 +137,13 @@ pixelBoard.addEventListener("click", colorePixel);
 
 /** ======= Função Do Button Clear ======= */
 
-// funciona, mas não passa no teste;
-
 function clearBoard (){
 
   const listaPixelBoard = document.querySelectorAll(".pixel");
   
   for(let i = 0; i < listaPixelBoard.length; i += 1){
     listaPixelBoard[i].style.backgroundColor = "white";
-    console.log(listaPixelBoard[i].style.backgroundColor = "white");
+    
   }
 }
 buttonClear.addEventListener("click", clearBoard);
